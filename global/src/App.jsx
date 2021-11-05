e65262fa865f391212270818fccd85cb32b2ef58
Vinny-PressF <viniciufa@gmail.com>
2021-11-05 16:22:24 -0300

    Preparando ambiente e criando a pasta onde ficaram as imagens

--- global/src/App.jsx
+++ global/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import SecondBody from './components/home/body/secondBody/SecondBody';
 
 function App() {
   return (
--- global/src/components/home/body/secondBody/secondBody.jsx
+++ global/src/components/home/body/secondBody/secondBody.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function secondBody() {
+export default function SecondBody() {
     return (
         <div>
             
