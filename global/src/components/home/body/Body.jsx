467bcf85428c366d2161cbe6edc54bc66d8ee5e8
JoaoPorto23 <rm87082@fiap.com.br>
2021-11-06 11:44:59 -0300

    Finalização do ThirdBody

--- global/src/components/home/body/Body.jsx
+++ global/src/components/home/body/Body.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import MainBody from './mainBody/MainBody'
 import SecondBody from './secondBody/SecondBody'
+import ThirdBody from './thirdBody/ThirdBody'
 
 export default function Body() {
     return (
         <div>
             <MainBody/>
             <SecondBody/>
+            <ThirdBody/>
         </div>
     )
 }
--- global/src/components/home/body/thirdBody/thirdBody.jsx
+++ global/src/components/home/body/thirdBody/thirdBody.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import { SectionStyled, FoodStyled, InfoStyled } from './styled.js'
+import FoodForAll from "../../../../assets/images/FoodForAll.jpg"
 
-export default function thirdBody() {
+export default function ThirdBody() {
     return (
-        <div>
-            
-        </div>
+        <SectionStyled>
+            <div className="container">
+                <FoodStyled>
+                    <div className="food">
+                        <h1>FOOD</h1>
+                        <h2>FOR ALL!</h2>
+                        <div className="food__img">
+                            <img alt="Comida para Todos" src={FoodForAll}/>
+                        </div>
+                    </div>
+                </FoodStyled>
+                <InfoStyled>
+                    <div className="title__info">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis doloribus consequuntur voluptatem nihil, provident, expedita quidem, quaerat inventore perspiciatis id quo consectetur amet aperiam laboriosam earum? Fugit soluta quidem ea!</p>
+                    </div>
+                    <div className="info__pai">
+                        <div className="info__filha">
+                            <h1>DESGIN</h1>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate ab earum doloribus voluptatem ipsam excepturi odit blanditiis exercitationem, perspiciatis officia soluta aliquam dolores error, quae vitae quod placeat nam corrupti?</p>
+                        </div>
+                        <div className="info__filha">
+                        <h1>DESGIN</h1>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate ab earum doloribus voluptatem ipsam excepturi odit blanditiis exercitationem, perspiciatis officia soluta aliquam dolores error, quae vitae quod placeat nam corrupti?</p>
+                        </div>
+                    </div>
+                    <div className="info__pai">
+                        <div className="info__filha">
+                            <h1>DESGIN</h1>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate ab earum doloribus voluptatem ipsam excepturi odit blanditiis exercitationem, perspiciatis officia soluta aliquam dolores error, quae vitae quod placeat nam corrupti?</p>
+                        </div>
+                        <div className="info__filha">
+                        <h1>DESGIN</h1>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate ab earum doloribus voluptatem ipsam excepturi odit blanditiis exercitationem, perspiciatis officia soluta aliquam dolores error, quae vitae quod placeat nam corrupti?</p>
+                        </div>
+                    </div>
+                    <div className="title__info">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis doloribus consequuntur voluptatem nihil, provident, expedita quidem, quaerat inventore perspiciatis id quo consectetur amet aperiam laboriosam earum? Fugit soluta quidem ea!</p>
+                    </div>
+
+                </InfoStyled>
+            </div>
+        </SectionStyled>
     )
 }
