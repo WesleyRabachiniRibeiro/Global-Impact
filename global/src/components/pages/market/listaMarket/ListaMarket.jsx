e2e00b2c959e1592dfe99464c5265c251828de14
JoaoPorto23 <rm87082@fiap.com.br>
2021-11-11 18:09:47 -0300

    Utilizando props no Dashboard

--- global/src/components/pages/market/listaMarket/ListaMarket.jsx
+++ global/src/components/pages/market/listaMarket/ListaMarket.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import { BsBook } from 'react-icons/bs'
-import { AiOutlineEye } from 'react-icons/ai'
 import { ListaStyled } from '../styled'
 
-export default function ListaMarket() {
+export default function ListaMarket(props) {
     return (
         <ListaStyled>
-        <p>Supermercado</p>
+        <p>Supermercados</p>
             <nav>
                 <ul>
-                    <li>Supermercado One<div className='gap'><BsBook/><span>190,174</span></div></li>
-                    <li>Supermercado Two<div className='gap'><BsBook/><span>190,174</span></div></li>
-                    <li>Supermercado Three<div className='gap'><BsBook/><span>190,174</span></div></li>
-                    <li>Supermercado Four<div className='gap'><BsBook/><span>190,174</span></div></li>
+                    <li>{props.supermarketNameOne}<div className='gap'><BsBook/><span>190,174</span></div></li>
                 </ul>
             </nav>
         </ListaStyled>
