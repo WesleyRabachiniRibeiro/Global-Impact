8c206143797f7604bf9c3f21537f3315d2912566
JoaoPorto23 <rm87082@fiap.com.br>
2021-11-16 15:14:46 -0300

    Finalizando o Estoque de Alimentos

--- global/src/components/pages/dashboard/Dashboard.jsx
+++ global/src/components/pages/dashboard/Dashboard.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import MarketBoard from '../market/MarketBoard'
 import MenuBar from '../menuBar/MenuBar'
 import TableProduct from '../tableProduct/TableProduct'
+import FoodStock from '../foodStock/FoodStock'
 
 export default function Dashboard() {
     return (
         <>
-        <MenuBar firstCount="Produtos recebidos" secondCount="Produtos doados" thirdCount="Produtos vencidos" component={<MarketBoard/>}/>
+        <MenuBar firstCount="Produtos recebidos" secondCount="Produtos doados" thirdCount="Produtos vencidos" component={<FoodStock/>}/>
         </>
     )
 }
